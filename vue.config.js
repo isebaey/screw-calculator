668469806a5c0c3e962f65a37f16d99d6be17586
Ahmad El-Sebaey <102492335+isebaey@users.noreply.github.com>
2024-09-18 15:56:08 +0300

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/screw-calculator/" : "/",
   transpileDependencies: true,
   pwa: {
     name: "Scrow Calculator",
